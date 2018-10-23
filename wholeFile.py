9b1716f946286948c4e3f0e7d4f4d15415d190a0
sgspectra <astorch13@gmail.com>
2018-10-22 22:18:04 -0400

    added comments to wholeFile.py

--- wholeFile.py
+++ wholeFile.py
@@ -1,3 +1,8 @@
+# @reFileName is the name of the file containing regular expressions to be searched for.
+# expressions are separated by newline
+# @fileToScanName is the name of the file that you would like to run the regex against.
+# It is read all at once and the results returned in @match
+
 import re
 
 reFileName = input("Please enter the name of the file containing regular expressions:")
