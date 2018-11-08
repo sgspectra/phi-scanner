d562d2b5573a7f2a9a398b914c5f5986b9528382
Andrew Storch <astorch13@gmail.com>
2018-11-07 20:10:27 -0500

    added a sample directory to navigate through using FileFinder.py

---
+++ FileFinder.py
@@ -0,0 +1,16 @@
+import os
+
+
+def findfiles(dir):
+    for entry in os.scandir(dir):
+        if entry.is_dir(follow_symlinks=False):
+            findfiles(entry.path)
+        else:
+            print(entry)
+
+
+print("Please enter the directory you wish to find files in:")
+path = input()
+findfiles(path)
+
+
