ec02409e8e8f321372cc393fda9c05cc0e69bf78
Andrew Storch <astorch13@gmail.com>
2018-11-07 20:23:17 -0500

    had output of FileFinder write to a lib file

--- FileFinder.py
+++ FileFinder.py
@@ -2,11 +2,15 @@ import os
 
 
 def findfiles(dir):
+    output = open('./lib/textFiles.txt', 'a+')
     for entry in os.scandir(dir):
         if entry.is_dir(follow_symlinks=False):
             findfiles(entry.path)
         else:
             print(entry.path)
+            output.write(entry.path)
+            output.write('\n')
+    output.close()
 
 
 print("Please enter the directory you wish to find files in:")
